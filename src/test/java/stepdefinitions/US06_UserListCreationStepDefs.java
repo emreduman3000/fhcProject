ae597dca0ad0d3f4ec6f42b1be4dfcc479ea05ab
Kerim5858 <kerimanulur@gmail.com>
2020-08-18 23:27:32 -0400

    first commit from Kerim  Master

--- src/test/java/stepdefinitions/US06_UserListCreationStepDefs.java
+++ src/test/java/stepdefinitions/US06_UserListCreationStepDefs.java
@@ -1,4 +1,11 @@
 package stepdefinitions;
 
+import org.testng.annotations.Test;
+
 public class US06_UserListCreationStepDefs {
+    @Test
+    public void test(){
+        System.out.println("Hi everyone from Kerim Anulur");
+
+    }
 }
